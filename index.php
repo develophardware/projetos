8f775bc1ee01e6b9c2e62a48d0d6ebb69eb32681
Paulo Rodrigues <poolldrigues@outlook.com>
2023-03-14 16:36:54 -0300

    Menu

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Olá, Mundo!</title>
 </head>
@@ -13,13 +14,9 @@
     <header>
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
-                DevelopHarware
+                <img src="./img/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""> DevelopHardware
             </a>
-        </nav>
-        <menu>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -29,18 +26,30 @@
                             <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Destaques</a>
+                            <a class="nav-link" href="./">JavaScript</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Preços</a>
+                            <a class="nav-link" href="#">PHP</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Desativado</a>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Link dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Ação</a>
+                                <a class="dropdown-item" href="#">Outra ação</a>
+                                <a class="dropdown-item" href="#">Algo mais aqui</a>
+                            </div>
                         </li>
+
                     </ul>
                 </div>
             </nav>
-        </menu>
+        </nav>
+
+
+
     </header>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
