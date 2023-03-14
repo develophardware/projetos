9fc85819296158d4de1245781f9c3e689c7cac0f
Paulo Rodrigues <poolldrigues@outlook.com>
2023-03-14 16:47:53 -0300

    Menu Projetos

--- index.php
+++ index.php
@@ -25,19 +25,32 @@
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./">JavaScript</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">PHP</a>
-                        </li>
-
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Link dropdown
-                            </a>
+                        JavaScript
+                      </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Ação</a>
+                                <a class="dropdown-item" href="./Calculo-IMC/index.html">Calculo IMC</a>
+                                <a class="dropdown-item" href="/Projeto-Lampada/index.html">Projeto Lampada</a>
+                                <a class="dropdown-item" href="#">Algo mais aqui</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        PHP
+                      </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="./Calculo-IMC/index.html">Calculo IMC</a>
+                                <a class="dropdown-item" href="#">Outra ação</a>
+                                <a class="dropdown-item" href="#">Algo mais aqui</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        CSS
+                      </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="./Calculo-IMC/index.html">Calculo IMC</a>
                                 <a class="dropdown-item" href="#">Outra ação</a>
                                 <a class="dropdown-item" href="#">Algo mais aqui</a>
                             </div>
@@ -51,8 +64,9 @@
 
 
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
