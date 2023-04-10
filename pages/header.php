56fcf9334cc52f361608c42e89a5a7777b19d67e
Paulo Rodrigues <paulinhochok@hotmail.com>
2023-04-09 23:07:06 -0300

    Atualizando Menu

--- pages/header.php
+++ pages/header.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <header>
+    <header style="margin: 1em;">
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
                 <img src="../img/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""> DevelopHardware
