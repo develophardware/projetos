921ea5afb2e487a86f03d6ecf3f7c16bf9c914b7
Paulo Rodrigues <paulinhochok@hotmail.com>
2023-04-09 23:31:53 -0300

    Atualizando Menu

--- pages/header.php
+++ pages/header.php
@@ -27,7 +27,7 @@
                             <a class="nav-link text-light bg-dark rounded-pill" href="#">Inicio <span class="sr-only"></span></a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-light bg-dark rounded-pill" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Projetos
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -37,10 +37,10 @@
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre mim</a>
+                            <a class="nav-link text-light bg-dark rounded-pill" href="#">Sobre mim</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contato</a>
+                            <a class="nav-link text-light bg-dark rounded-pill" href="#">Contato</a>
                         </li>
 
                     </ul>
