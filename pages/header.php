e4858ff39f416efbce8397ad91376e28731cc604
Paulo Rodrigues <paulinhochok@hotmail.com>
2023-05-03 22:10:32 -0300

    Alteração de diretorios

--- pages/header.php
+++ pages/header.php
@@ -37,7 +37,7 @@
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre</a>
+                            <a class="nav-link" href="#">Sobre mim</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contato</a>
