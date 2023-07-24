0f5f32d7856d03cdd3065079d688bd60920a19cb
Paulo Rodrigues <paulinhochok@hotmail.com>
2023-07-23 23:01:29 -0300

    Incluindo Menu

--- pages/header.php
+++ pages/header.php
@@ -11,46 +11,8 @@
 </head>
 
 <body>
-    <header style="margin: 1em;">
-        <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""> DevelopHardware
-            </a>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <!-- <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a> -->
-                            <a class="nav-link text-light bg-dark rounded-pill" href="#">Home <span class="sr-only"></span></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Projetos
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" target="_blank" href="/Calculo-IMC/index.php">Calculo IMC</a>
-                                <a class="dropdown-item" target="_blank" href="/Projeto-Lampada/index.php">Projeto Lampada</a>
-
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre mim</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contato</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-        </nav>
-    </header>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-
+   <?php 
+    require_once './menu.php';
+   
+   ?>
 </html>
