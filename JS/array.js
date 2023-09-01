4beac75d3637c1205bcd45092a399a8d428994b6
develophardware <poolldrigues@outlook.com>
2023-09-01 11:20:11 -0300

    Cusro em video

---
+++ JS/array.js
@@ -0,0 +1,32 @@
+const alunos = ['lucas', 'maria', 'joão']
+
+console.log(alunos)
+
+alunos.push('merlon') //adiciona no final
+
+console.log(alunos)
+
+// adiciona no começo 
+
+alunos.unshift('luli')
+
+console.log(alunos)
+
+// remove do final 
+
+alunos.pop()
+
+console.log(alunos)
+
+// remove do começo 
+alunos.shift()
+
+console.log(alunos)
+
+// apagar indice inteiro 
+
+delete alunos[1]
+
+console.log(alunos)
+
+
