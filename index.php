3719f5eec92d35e5c12c9308ca5995634b0324c8
Paulo Rodrigues <paulinhochok@hotmail.com>
2023-11-23 03:51:53 -0300

    Adicionado Sessao Habilidades e Sobre

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <title>DevelopHardware</title>
 
 </head>
@@ -67,6 +69,54 @@
             <!-- interface-->
         </section>
         <!-- topo-site-->
+
+        <section class="habilidades">
+            <div class="interface">
+                <h2>MINHAS <span>HABILIDADES</span> </h2>
+                <div class="flex"><!--flex-->
+                    <div class="habilidades-box">
+                        <i class="bi bi-code-slash"></i>
+                        <h3>Desenvolvimento</h3> 
+                        <p>Desenvolvimento de web-sites e aplicações web.</p>
+                    </div><!--habilidades-box-->  
+                    <div class="habilidades-box">
+                        <i class="bi bi-robot"></i>
+                        <h3>Automação</h3> 
+                        <p>Automatizando processos através de tecnologia.</p>
+                    </div><!--habilidades-box-->  
+                    <div class="habilidades-box">
+                        <i class="bi bi-motherboard"></i>
+                        <h3>Hardware Software e Suporte</h3> 
+                        <p>Manutenção e suporte de sistemas computacionais.</p>
+                    </div><!--habilidades-box-->  
+                </div><!--flex-->               
+            </div><!--interface-->
+        </section><!--habilidades-->
+
+        <section class="sobre">
+            <div class="interface">
+                <div class="flex">
+                    <div class="img-sobre">
+                        <img src="img/me.png" alt="Foto sobre eu">
+                    </div><!-- img-sobre-->
+                    <div class="txt-sobre">
+                        <h2>MUITO PRAZER, <span> SOU PAULO RODRIGUES. </span></h2>
+                        <p>Com uma formação sólida em Ciência da Computação e uma trajetória consolidada como Analista de Suporte, trago uma expertise abrangente na resolução de desafios de software e hardware. Minha atuação se destaca na manutenção e aprimoramento da confiabilidade, segurança e operabilidade do ambiente de TI, garantindo a eficiência do parque de máquinas.</p>
+                        <p>Minhas habilidades abrangem uma variedade de tecnologias e competências, desde a administração de sistemas operacionais como Windows, Windows Server e Linux até a gestão avançada de ambientes virtuais, incluindo Virtualização Hyper-v e gerenciamento via Active Directory. Além disso, possuo experiência sólida em hardware, servidores, e especialização em Cabeamento Estruturado, contribuindo para uma infraestrutura robusta e eficaz.</p>
+                        <p>No âmbito do desenvolvimento, minha expertise em PHP, JavaScript, CSS, HTML e Bootstrap me permite criar soluções inteligentes, enquanto minha habilidade em RPA (Automação de Processos Robóticos) com UiPath aprimora a eficiência operacional.</p>
+                        <p>Estou comprometido em aprimorar constantemente minhas habilidades, recentemente embarcando em uma jornada para adquirir proficiência em serviços de nuvem, incluindo AWS, Google Cloud e Azure. Essa busca contínua por atualização e aprimoramento reflete minha dedicação em oferecer soluções inovadoras e alinhadas com as mais recentes tendências tecnológicas.</p>
+                        <p>Estou entusiasmado em enfrentar novos desafios e contribuir significativamente para equipes e projetos que demandem conhecimento, experiência e paixão pela excelência em TI e desenvolvimento de software.</p>
+                        <div class="btn-social">
+                            <a href="https://www.linkedin.com/in/poldrigues/" target="_blank" ><button><i class="bi bi-linkedin"></i></button></a>
+                            <a href="https://github.com/develophardware" target="_blank"><button><i class="bi bi-github"></i></button></a>
+                            <a href="https://www.instagram.com/develophardware/" target="_blank"><button><i class="bi bi-instagram"></i></button></a>
+                            <a href="#c" target="_blank"><button><i class="bi bi-youtube"></i></button></a>
+                        </div><!--btn-social-->
+                    </div><!--txt-sobre-->
+                </div><!-- Flex-->
+            </div><!-- Interface-->
+        </section><!-- Sobre-->
+
     </main>
 
 </body>
